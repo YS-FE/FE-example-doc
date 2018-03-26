60c391dc7131c7b9cc69f994d4e048f1bfe93780
yangshang01 <yangshang01@58ganji.com>
2018-03-26 17:43:10 +0800

    add vue

---
+++ vue/vue-async-load.js
@@ -0,0 +1,27 @@
+
+//全局组件
+Vue.component('async-example', function (resolve, reject) {
+    axios.get('/xxx').then(data => {
+     //data是个对象 {template: `<h1>sgsgsg</h1>`}
+     resolve(data); 
+    })
+ })
+ 
+ //局部组件
+ new Vue({
+     ....,
+     components: {
+         xxx: function(resolve, reject){
+               axios.get('/xxx').then(data => {
+            //data是个对象 {template: `<h1>sgsgsg</h1>`}
+            
+             resolve(data); //resolve(Vue.extend(data)也可以
+            })
+         }
+     }
+ })
+
+ 
+ //使用webpack实现异步加载
+ const xxx = () => import ('./xxx')
+ const yyy = (resolve) => require(['./yyy'], resolve)
